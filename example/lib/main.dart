4c374fba1d8badf47f5c6c127cc14fedd6e16806
reidha <kdaimon.8@gmail.com>
2022-01-16 21:02:37 +0800

    0.1.0+1 2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,6 +8,11 @@ void main() {
       DefaultMap<int, HashMap<int, int>>(() => HashMap<int, int>());
   final DefaultMap<int, List<int>> m3 = DefaultMap<int, List<int>>();
 
+  m1[0] = 1;
+
+  m2[0][0] = 0;
+  m2[1][100] = 100;
+
   m3[0].add(1);
   m3[1].add(2);
   m3[1].add(1);
